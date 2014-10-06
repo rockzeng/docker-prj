94fa0021af790f30c9c3179eb51de4930e951af7
ehkim <ekim@riotgames.com>
2014-10-06 17:56:15 +0900

    first commit

---
+++ src/main/java/com/example/docker_prj/App.java
@@ -0,0 +1,13 @@
+package com.example.docker_prj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
